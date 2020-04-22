aea6e3ad34c0e062ca7c0870717ca76f61f8ae39
Dynamoviec <dynamoviec@gmail.com>
2020-04-22 17:26:24 +0300

    Probelu

---
+++ task2string/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task2string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your text:");
+            string text = Console.ReadLine();
+            for (int i = 0; i < text.Length; i++)
+            {
+                text = text.Replace("  ", " ");
+            }
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
